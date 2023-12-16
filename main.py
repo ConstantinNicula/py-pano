f1ae8cd701ded461510947fa261d42903fbc231e
Constantin Nicula <constantin.nicula97@gmail.com>
2023-12-17 01:08:38 +0200

    Refactored matcher

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import loader
+import time 
+from matcher import Matcher
 
 def main():
     parser = argparse.ArgumentParser()
@@ -7,8 +9,10 @@ def main():
     parser.add_argument("out_folder", help="Path where result will be stored")
     args = parser.parse_args()
 
-    data, exif = loader.load_batch(args.img_src_folder)
-    print (exif)
+    imgs, imgs_exif = loader.load_batch(args.img_src_folder)
+    matcher = Matcher()
+    t1 = time.time()
+    matcher.match(imgs)
 
 if __name__ == "__main__":
     main()
--- matcher.py
+++ matcher.py
@@ -1,8 +1,7 @@
 import cv2 
 import time
 import numpy as np
-import numpy.linalg
-import itertools
+from utils import * 
 
 class Matcher: 
     def __init__(self, kpts_per_img:int = 2000): 
@@ -14,14 +13,45 @@ class Matcher:
         self.img_descriptors = []
         self.img_keypoints = [] 
         
-        # Contains matches between keypoints in img_i and img_k  where k > i 
-        # Results are filtered and contain matches only for valid overlaps
-        self.img_matches = [] 
+        # Contains matches between tuples which describe image overlaps:  
+        # hash[(i,j)] = (H, inlier_matches)
+        # i, j - indices of overlapping images
+        # H - estimated homography
+        # inlier_matches - ndarray of inliers matches [(k, p) ...] 
+        # keypoint k in image i matches with keypoint p in image j  
+        self.img_overlap_data = {}
+        
+        # Pairs (i, j) of overlapping images
+        self.img_connectivity = []
 
-    """
-        Detect overlapping images.
-    """
+    def get_overlapping_images(self):
+        """
+            Returns a list of tuples containing valid image overlaps
+        """ 
+        return self.img_connectivity
+
+    def get_overlap_data(self, img1_id: int, img2_id: int) -> tuple[np.ndarray, np.ndarray, np.ndarray] | None: 
+        """
+            Returns tuple (H, kp1, kp2) containing overlap data between img1 and img2 or None if no overlap exists
+            H - homography from img1 to img2
+            kp1 - list of keypoints in img1 
+            kp2 - list of keypoints in img2
+        """
+        if img2_id > img1_id:
+            img1_id, img2_id = img2_id, img1_id
+
+        if (img1_id, img2_id) not in self.img_overlap_data:
+            return None
+
+        H, matches = self.img_overlap_data[(img1_id, img2_id)]  
+        img1_kp = self.img_keypoints[img1_id][matches[:, 0]]
+        img2_kp = self.img_keypoints[img2_id][matches[:, 1]]
+        return (H, img1_kp, img2_kp)
+
     def match(self, imgs: list[np.ndarray], m:int = 6): 
+        """
+            Detect overlapping images.
+        """
         # preprocess all images
         for img in imgs: 
             keypoints, descriptors = self.__get_keypoints(img)
@@ -30,7 +60,6 @@ class Matcher:
 
         # Quadratic matching between all images. 
         # An approximate solution can be obtained in n*log(n) but it's more difficult to implements 
-        self.img_matches = []  
         for i in range(len(imgs) - 1):
             for j in range(i + 1, len(imgs)):
                 kp_raw_matches = self.__match_keypoints(self.img_descriptors[i], self.img_descriptors[j])
@@ -38,52 +67,54 @@ class Matcher:
                 # filter using geometric check
                 valid, H, inliner_kp_matches = self.__is_valid_overlap(self.img_keypoints[i], self.img_keypoints[j], kp_raw_matches)
                 if valid:
-                    print(f"Found valid match {i} -> {j}") 
-                    self.img_matches.append((i, j, inliner_kp_matches))                    
+                    if DEBUG_ENABLED(): print(f"Found valid match {i} -> {j}") 
+                    self.img_overlap_data[(i, j)] = (H, inliner_kp_matches)
+        
+        self.img_connectivity = self.img_overlap_data.keys()
 
-        return self.img_matches
-
-    """
-        Extracts ORB keypoints and descriptors
-        Returns tuple of two ndarrays:
-            - kps.shape = (n, 2)
-            - descriptor.shape = (n, 32) 
-    """
     def __get_keypoints(self, img: np.ndarray) -> tuple[np.ndarray, np.ndarray]: 
+        """
+            Extracts ORB keypoints and descriptors
+            Returns tuple of two ndarrays:
+                - kps.shape = (n, 2)
+                - descriptor.shape = (n, 32) 
+        """
         # convert image to gray scale
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         cv_kpts, desc = self.kp_detector.detectAndCompute(img_gray, None) 
         return np.array([kp.pt for kp in cv_kpts]), desc 
 
-    """
-        Matches two sets of ORB descriptors 
-        Returns ndarray of shape (n, 2):
-            - match[:, 0] -> index of train kp
-            - match[:, 1] - > index of query kp
-    """
     def __match_keypoints(self, desc1: np.ndarray, desc2: np.ndarray) -> np.ndarray:
+        """
+            Matches two sets of ORB descriptors 
+            Returns ndarray of shape (n, 2):
+                - match[:, 0] -> index of train kp
+                - match[:, 1] - > index of query kp
+        """
         # desc1 - query image, desc2 - train image
         kpt_matches = self.kp_matcher.knnMatch(desc1, desc2, k=2)
         good_kpt_matches = []
+
         for m in kpt_matches:
             if len(m) == 2 and m[0].distance < 0.7 * m[1].distance:
                 good_kpt_matches.append(m[0])
         return np.array([(m.queryIdx, m.trainIdx) for m in good_kpt_matches])
 
-    """
-        Determine if there is a valid homography between kpts1 and kpts2, based on provided matches 
-        Returns a tuple containing (valid_overlap, est_homography, kpts_inliers)
-            - valid_overlap - bool indicating if there is a valid homography between the provided points
-            - est_homography - calculated homography matrix 
-            - kpts_inliers - filtered keypoints 
-    """
 
     def __is_valid_overlap(self, kpts1: np.ndarray, kpts2: np.ndarray, matches: np.ndarray, alpha = 8.0, beta = 0.3):
+        """
+            Determine if there is a valid homography between kpts1 and kpts2, based on provided matches 
+            Returns a tuple containing (valid_overlap, est_homography, kpts_inliers)
+                - valid_overlap - bool indicating if there is a valid homography between the provided points
+                - est_homography - calculated homography matrix 
+                - kpts_inliers - filtered keypoints 
+        """
         if len(matches) < 4:
             return False, None, None
 
         H, inliner_mask = self.__estimate_homography_ransac(kpts1, kpts2, matches) 
         nf, ni = len(matches), len(inliner_mask)
+        if DEBUG_ENABLED(): print(f"total matches {nf}, valid matches {ni}")
         valid = ni > alpha + beta * nf
         return valid, H, matches[inliner_mask]
 
@@ -118,11 +149,10 @@ class Matcher:
                 # store best model
                 H = Hi
                 max_num_inliers, err_std, inlier_mask = cur_num_inliers, curr_std, curr_inlier_mask
-                # print(f"Iter {i} nr_matches:{cur_num_inliers}, err_std:{curr_std}")
             
             # update N            
             e = 1 - cur_num_inliers / len(matches) + 1e-9
-            N = min(max_iters, int(np.log(1-0.99)/ np.log(1-(1-e)**4)))
+            N = min(max_iters, int(np.log(1 - 0.99)/ np.log(1 - (1 - e)**4)))
 
             i += 1
         return H, inlier_mask
@@ -167,19 +197,3 @@ class Matcher:
         errors = norm_err[inlier_mask]
 
         return len(inlier_mask), np.std(errors), inlier_mask
-
-    def __utils_resize(self, img: np.ndarray, scale = 0.5):
-        new_dims = (int(img.shape[1] * scale), int(img.shape[0] * scale)) 
-        return cv2.resize(img, new_dims)
-
-import loader
-if __name__ == "__main__":
-    imgs, exif = loader.load_batch("./input_imgs/field")
-    # imgs, exif = loader.load_batch("./input_imgs/grandesjorasses")
-    # imgs, exif = loader.load_batch("./input_imgs/apron")
-    matcher = Matcher()
-    t1 = time.time()
-    matcher.match(imgs)
-    print(f"Delta time {time.time() - t1}")
-    #matcher.test_extract(imgs[0])
-    #res = matcher.test_match(imgs[0], imgs[2])
