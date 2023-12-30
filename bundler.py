dc462def320821f94a27b5cfdf8b5d7af4817530
Constantin Nicula <constantin.nicula97@gmail.com>
2023-12-30 17:25:53 +0200

    Implemented focal length estimation

--- bundler.py
+++ bundler.py
@@ -7,7 +7,6 @@ class Bundler:
         self.img_data = {}
         self.img_overlap_data = []
         
-        
     def add_image(self, img_id:int, img: np.ndarray):
         # no need to store the actual image, store internal id and image shape
         self.img_data[img_id] = [-1, img.shape]
@@ -31,7 +30,8 @@ class Bundler:
         return ret
     
     def optimize(self):
-        #https://imkaywu.github.io/blog/2017/10/focal-from-homography/
+        # https://imkaywu.github.io/blog/2017/10/focal-from-homography/
+        # https://stackoverflow.com/questions/71035099/how-can-i-call-opencv-focalsfromhomography-from-python
         x0 = np.zeros(((self.used_img_cnt-1) * 4 ))
         x0[0:3] = [0.1 +5.23598704e-01, 0.05+5.23598658e-01,  6.63581180e-10]
         x0[3] =500 
@@ -94,25 +94,77 @@ def skew(x: np.ndarray) -> np.ndarray:
                     [x[2], 0, -x[0]],
                     [-x[1], x[0], 0]])
 
+def estimate_focal_length(M: np.ndarray) -> tuple[float|None, float|None]:
+    """
+        Estimates focal lengths based on the mapping matrix M (it is special case of homography matrix)
+        which can be decomposed into M = V_1 * R_10 * V_0^-1
+        Returns tuple of focal lengths
+    """
+    assert M.shape == (3, 3)
+    m = np.ravel(M) 
+
+    # estimated focal lengths 
+    f0, f1 = None, None
+
+    # Compute f0
+    d1 = m[0] * m[3] + m[1] * m[4]
+    d2 = m[0] * m[0] + m[1] * m[1] - m[3] * m[3] - m[4] * m[4]
+
+    v1 = -m[2] * m[5] / d1 if d1 != 0.0 else -1.0
+    v2 = (m[5] * m[5] - m[2] * m[2]) / d2 if d2 != 0.0 else -1.0
+
+    if v1 < v2: 
+        v1, v2 = v2, v1
+        d1, d2 = d2, d1
+    if v1 > 0 and v2 > 0: f0 = np.sqrt(v1 if abs(d1) > abs(d2) else v2)
+    elif v1 > 0: f0 = np.sqrt(v1) 
+
+    # Compute f1
+    d1 = m[6] * m[7]
+    d2 = (m[7] - m[6]) * (m[7] + m[6])
+
+    v1 = -(m[0] * m[1] + m[3] * m[4]) / d1 if d1 != 0.0 else -1.0
+    v2 = (m[0] * m[0] + m[3] * m[3] - m[1] * m[1] - m[4] * m[4]) / d2 if d2 != 0.0 else -1.0
+
+    if v1 < v2: 
+        v1, v2 = v2, v1
+        d1, d2 = d2, d1
+    if v1 > 0 and v2 > 0: f1 = np.sqrt(v1 if abs(d1) > abs(d2) else v2)
+    elif v1 > 0: f1 = np.sqrt(v1) 
+    return (f0, f1)
+
+def test_focal_est():
+    f0 = np.random.randint(34, 200)
+    f1 = np.random.randint(34, 500)
+
+    V0 = np.array([[f0, 0, 0], [0, f0, 0], [0, 0, 1]])
+    V1 = np.array([[f1, 0, 0], [0, f1, 0], [0, 0, 1]])
+    R01 = create_rot_mat(np.random.rand(3) * 2 * np.pi)
+    M = V1 @ R01 @ np.linalg.inv(V0)
+    
+    print(f"Real values {f0=:}, {f1=:}")
+    ef0, ef1 = estimate_focal_length(M)
+    print(f"Estimated values {ef0=:}, {ef1=:}")
 
 if __name__ == "__main__":
-    bundler = Bundler()
-    h, w = 1000, 1000
-    bundler.add_image(0, np.zeros((h, w)))
-    bundler.add_image(1, np.zeros((h, w)))
+    test_focal_est()
+    # bundler = Bundler()
+    # h, w = 1000, 1000
+    # bundler.add_image(0, np.zeros((h, w)))
+    # bundler.add_image(1, np.zeros((h, w)))
 
-    f = 700
-    K = create_cam_mat(f, w/2, h/2) 
-    R = create_rot_mat(np.pi/6 * np.array([1, 1, 0]))
-    pts = []
-    for i in range(100):
-        pts.append([np.random.randint(0, w/2), np.random.randint(0, h/2), 1]) 
-    p1 = np.array(pts)
+    # f = 700
+    # K = create_cam_mat(f, w/2, h/2) 
+    # R = create_rot_mat(np.pi/6 * np.array([1, 1, 0]))
+    # pts = []
+    # for i in range(100):
+    #     pts.append([np.random.randint(0, w/2), np.random.randint(0, h/2), 1]) 
+    # p1 = np.array(pts)
 
-    #p1 = np.array([[100, 100, 1], [200, 200, 1], [300, 300, 1], [400, 400, 1]])
-    p2 = (K @ R @ np.eye(3) @ np.linalg.inv(K) @ p1.T).T
-    p2 = (p2.T / p2[:, 2]).T
-    print(p1, '\n', p2)
+    # #p1 = np.array([[100, 100, 1], [200, 200, 1], [300, 300, 1], [400, 400, 1]])
+    # p2 = (K @ R @ np.eye(3) @ np.linalg.inv(K) @ p1.T).T
+    # p2 = (p2.T / p2[:, 2]).T
+    # print(p1, '\n', p2)
 
-    bundler.add_overlapping_points(0, 1, p1, p2 )
-    bundler.optimize()
+    # bundler.add_overlapping_points(0, 1, p1, p2 )
+    # bundler.optimize()
