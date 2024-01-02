fcc1aba842b7b6f5a9df870e07b83bd211aec2ab
Constantin Nicula <constantin.nicula97@gmail.com>
2024-01-02 21:05:38 +0200

    Improvement: Implemented homogrpahy refinement

--- homography.py
+++ homography.py
@@ -6,8 +6,9 @@ def estimate_homography_ransac(kpts1: np.ndarray, kpts2: np.ndarray, reproj_err=
     H, inlier_mask = None, None
     
     num_keypoints = len(kpts1)
-    max_num_inliers = 0
+    num_inliers = 0
     err_std = np.Inf
+    err_mean = np.Inf
 
     i, N = 0, max_iters 
     while i < N:
@@ -20,30 +21,55 @@ def estimate_homography_ransac(kpts1: np.ndarray, kpts2: np.ndarray, reproj_err=
         Hi = estimate_homography(sel_kpts1, sel_kpts2) 
 
         # calculate the number of inliers
-        cur_num_inliers, curr_std, curr_inlier_mask = find_inliers(Hi, kpts1, kpts2, reproj_err) 
+        cur_num_inliers, curr_std, cur_err, curr_inlier_mask = find_inliers(Hi, kpts1, kpts2, reproj_err) 
         
         # update model if necessary
-        if cur_num_inliers > max_num_inliers or (cur_num_inliers == max_num_inliers and curr_std < err_std):
+        if cur_num_inliers > max(4, num_inliers) or (cur_num_inliers == num_inliers and curr_std < err_std):
             # store best model
+            num_inliers, err_std, err_mean, inlier_mask = cur_num_inliers, curr_std, cur_err, curr_inlier_mask
             H = Hi
-            max_num_inliers, err_std, inlier_mask = cur_num_inliers, curr_std, curr_inlier_mask
-        
-        # update N            
-        e = 1 - cur_num_inliers / num_keypoints + 1e-9
-        N = min(max_iters, int(np.log(1 - 0.99)/ np.log(1 - (1 - e)**4)))
-
         i += 1
+    
+    # improve estimate using all points
+    if H is not None: 
+        H = refine_estimate(kpts1[inlier_mask], kpts2[inlier_mask])
+        num_inliers, err_std, err_mean, inlier_mask = find_inliers(H, kpts1, kpts2, reproj_err)
     return H, inlier_mask
 
-def convert_to_homogenous(kpts: np.ndarray): 
-    ones_col = np.ones((len(kpts), 1))
-    return np.hstack((kpts, ones_col))
+def refine_estimate(kpts1: np.ndarray, kpts2: np.ndarray) -> np.ndarray:
+    cond_kpts1, T1 = normalize_input(kpts1)
+    cond_kpts2, T2 = normalize_input(kpts2)
+
+    H = estimate_homography(cond_kpts1, cond_kpts2)
+    H = np.linalg.inv(T2) @ H @ T1
+    H /= H[-1, -1]
+    return H
+
+def normalize_input(kpts: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    # convert to x,y from homogenous
+    xy = kpts[:, 0:2]
+
+    # compute center
+    t = np.mean(xy, axis=0)
+
+    # compute distances & scaling factor
+    dist = np.linalg.norm(xy - t, axis=1)
+    s = np.sqrt(2) / np.mean(dist, axis=0)
+    cond_mat = np.array([
+        [s, 0, -s*t[0]],
+        [0, s, -s*t[1]],
+        [0, 0,       1]
+    ])
+
+    cond_pts = transform_points(cond_mat, kpts)
+    return cond_pts, cond_mat
 
 def estimate_homography(kpts1: np.ndarray, kpts2: np.ndarray) -> np.ndarray:
-    assert len(kpts1) == len(kpts2) and len(kpts1) == 4
+    N = len(kpts1)
+    assert len(kpts1) == len(kpts2) and N >= 4
 
-    A = np.zeros((8, 9))
-    for i in range(4):
+    A = np.zeros((2 * N, 9))
+    for i in range(N):
         pta, ptb = kpts1[i], kpts2[i]
         
         # xi, yi, 1, 0, 0, 0, -xi' * xi, -xi' * yi, -xi' 
@@ -61,7 +87,7 @@ def estimate_homography(kpts1: np.ndarray, kpts2: np.ndarray) -> np.ndarray:
     # Extract solution from last column (normalized)
     return np.reshape(V[-1, :], (3, 3)) / V[-1,-1] 
 
-def find_inliers(H: np.ndarray, kpts1: np.ndarray, kpts2: np.ndarray, reproj_err: float) -> tuple[int, float, np.ndarray]: 
+def find_inliers(H: np.ndarray, kpts1: np.ndarray, kpts2: np.ndarray, reproj_err: float) -> tuple[int, float, float, np.ndarray]: 
     # compute transformed points
     pt_est = (H @ kpts1.T).T
     pt_est = (pt_est.T / pt_est[:, 2]).T
@@ -71,6 +97,15 @@ def find_inliers(H: np.ndarray, kpts1: np.ndarray, kpts2: np.ndarray, reproj_err
     norm_err = np.linalg.norm(err, axis=1)
 
     # extract inliers and the corresponding errors
-    inlier_mask = np.flatnonzero(norm_err < reproj_err)
+    inlier_mask = norm_err < reproj_err
     errors = norm_err[inlier_mask]
-    return len(inlier_mask), np.std(errors), inlier_mask
+    return np.count_nonzero(inlier_mask), np.std(errors), np.mean(errors), inlier_mask
+
+def convert_to_homogenous(kpts: np.ndarray): 
+    ones_col = np.ones((len(kpts), 1))
+    return np.hstack((kpts, ones_col))
+
+def transform_points(H: np.ndarray, kpts: np.ndarray) -> np.ndarray:
+    kpts_t = (H @ kpts.T).T
+    return (kpts_t.T / kpts_t[:, 2]).T    
+    
