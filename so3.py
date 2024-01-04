3c66021fe299f4351a46758d346155c7b07d7b7e
Constantin Nicula <constantin.nicula97@gmail.com>
2024-01-04 23:03:26 +0200

    Fixed singularity in so3 log

--- so3.py
+++ so3.py
@@ -9,11 +9,32 @@ def exp(phi: np.ndarray) -> np.ndarray:
     n = phi / t if t != 0.0 else np.array([1.0, 0.0, 0.0])
     return ct * np.eye(3) + (1 - ct) * np.outer(n, n)  + st * hat(n) 
 
+# def normalize_mat(R: np.ndarray):
+#     # https://math.stackexchange.com/questions/3292034/normalizing-a-quasi-rotation-matrix
+#     # https://stackoverflow.com/questions/23080791/eigen-re-orthogonalization-of-rotation-matrix
+#     return R * np.linalg.inv(np.sqrt(R.T @ R))
+
+
 def log(R: np.ndarray) -> np.ndarray:
         cos_angle = np.clip(0.5 * np.trace(R) - 0.5, -1.0, 1.0)
         angle = np.arccos(cos_angle)
+        
+        print(f"{angle=:} {R=:}")
         if np.isclose(angle, 0.): 
-            return vee(R - np.identity(3))
+            return np.zeros(3)
+
+        if np.isclose(angle, np.pi):
+            e0 = 1 
+            e1 = np.sign(R[0, 1]) 
+            e2 = np.sign(R[0, 2])
+            w = np.pi * np.array([
+                 e0 * np.sqrt(abs(0.5 * (1 + R[0, 0]))),
+                 e1 * np.sqrt(abs(0.5 * (1 + R[1, 1]))),
+                 e2 * np.sqrt(abs(0.5 * (1 + R[2, 2]))),
+            ])
+            return w
+            # return vee(R - np.identity(3))
+        print(np.sin(angle))
         return vee((0.5 * angle / np.sin(angle)) * (R - R.T))
 
 def hat(x: np.ndarray) -> np.ndarray:
